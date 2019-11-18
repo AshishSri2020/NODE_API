7c4fe08e6dcf48bcc9454a60ce71dbb0ecd786f7
AshishSri2020 <57796818+AshishSri2020@users.noreply.github.com>
2019-11-19 02:46:20 +0530

    Update NodeJSAPI.js

--- NodeJSAPI.js
+++ NodeJSAPI.js
@@ -64,8 +64,8 @@ app.get('/', async (request, response) => {
     response.end();
 })
 
-app.listen(process.env.port);
-return;
+//app.listen(process.env.port);
+//return;
 
 //========Get Method to get entry data
 async function FetchData() {
@@ -106,7 +106,7 @@ async function uploaddata(groupname, companyname, userid, fileName, content) {
     }
 }
 
-//FetchData() //.catch(handleError);;
+FetchData().catch(handleError);;
 ////FetchData(); 
 
 
