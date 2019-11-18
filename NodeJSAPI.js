510ad56bbb7f5bcd3761e12c6996df2151aa7512
AshishSri2020 <57796818+AshishSri2020@users.noreply.github.com>
2019-11-19 03:01:42 +0530

    Update NodeJSAPI.js

--- NodeJSAPI.js
+++ NodeJSAPI.js
@@ -1,15 +1,15 @@
 //=====to connect azure database
-const { CosmosClient } = require("@azure/cosmos");
-const endpoint = "https://sampledb123.documents.azure.com:443/"; // Add your endpoint
-const key = "[FhHc2ssWgaENG4IJDGQKGmAG5RYHSy90Gj8U7kFvuP7M31f0WOGR4nEEQHkYBrTEEypzvhGmeQQuv6ujSeCBUw==]"; // Add the masterkey of the endpoint
-const client = new CosmosClient({ endpoint, key });
+//const { CosmosClient } = require("@azure/cosmos");
+//const endpoint = "https://sampledb123.documents.azure.com:443/"; // Add your endpoint
+//const key = "[FhHc2ssWgaENG4IJDGQKGmAG5RYHSy90Gj8U7kFvuP7M31f0WOGR4nEEQHkYBrTEEypzvhGmeQQuv6ujSeCBUw==]"; // Add the masterkey of the endpoint
+//const client = new CosmosClient({ endpoint, key });
 
-const databaseid = "sample database";
-const containerId = "sample collection";
+//const databaseid = "sample database";
+//const containerId = "sample collection";
 
 
-var express = require('express');
-var app = express();
+//var express = require('express');
+//var app = express();
 
 //=====end azure database========
 
