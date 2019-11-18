3a5802cd8d25b8f4788af0393b132164f2383feb
AshishSri2020 <57796818+AshishSri2020@users.noreply.github.com>
2019-11-19 02:52:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-//http.createServer(function (req, res) {
-  //  res.writeHead(200, { 'Content-Type': 'text/plain' });    
-//    res.end('Hello World\n');
-//}).listen(port);
+http.createServer(function (req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });    
+    res.end('Hello World\n');
+}).listen(port);
 
 var cosmos = require('./NodeJSAPI');
